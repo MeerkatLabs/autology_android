00481118e87ddd694be4542fc6ad02f683342522
Robert Robinson <rerobins@gmail.com>
2017-11-21 08:23:28 -0700

    Working to get Settings installed
    - Preferences.xml file
    - Fragment for settings

---
+++ app/src/main/java/org/meerkatlabs/autology/fragments/SettingsFragment.java
@@ -0,0 +1,21 @@
+package org.meerkatlabs.autology.fragments;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+import org.meerkatlabs.autology.R;
+
+/**
+ *
+ */
+
+public class SettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
