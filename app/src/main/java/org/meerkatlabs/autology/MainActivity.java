a7ccbb97d87e0639251f639ada774fc5cf0bd891
Robert Robinson <rerobins@gmail.com>
2017-11-21 11:54:14 -0700

    Set Storage Directory
    - Settings will display the current storage location for the
      application.
    - Read the settings during initialization of the application
    - Cleaned up the Settings Activity/Fragment (Using a private class
      for the fragment).

--- app/src/main/java/org/meerkatlabs/autology/MainActivity.java
+++ app/src/main/java/org/meerkatlabs/autology/MainActivity.java
@@ -1,16 +1,25 @@
 package org.meerkatlabs.autology;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import org.meerkatlabs.autology.settings.SettingsActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int EXTERNAL_STORAGE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +29,27 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        requestPermissions();
+
+        // TODO: Check the status of the file system configured
+        // If the value is not provided for the storage directory, then create the value by appending
+        // autology to the getExternalDirectory().
+
+        // TODO: When the directory structure is changed, then need to reload the content of the
+        // list panel
+
+        // TODO: Storage Engine for the application, responsible for providing the contents of
+        // the directory log, adding/removing files from the log
+
+        // TODO: Move the STORAGE_DIRECTORY_KEY value to a new location, or provide a means of
+        // abstracting that content away so that the key isn't known to the other acivities.
+
         // Load up the preferences from the system
+        Log.i("RER", "Loading up the default preferences");
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        Log.i("RER", "Storage directory: " + preferences.getString(SettingsActivity.STORAGE_DIRECTORY_KEY, "Unknown Key"));
     }
 
     @Override
@@ -45,5 +73,21 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
 
+    }
+
+    private void requestPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    EXTERNAL_STORAGE_REQUEST);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
--- app/src/main/java/org/meerkatlabs/autology/settings/SettingsActivity.java
+++ app/src/main/java/org/meerkatlabs/autology/settings/SettingsActivity.java
@@ -1,12 +1,29 @@
 package org.meerkatlabs.autology.settings;
 
+import android.content.SharedPreferences;
+import android.os.Environment;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import com.afollestad.materialdialogs.folderselector.FolderChooserDialog;
 
 import org.meerkatlabs.autology.R;
 
-public class SettingsActivity extends AppCompatActivity {
+import java.io.File;
+
+public class SettingsActivity extends AppCompatActivity implements FolderChooserDialog.FolderCallback {
+
+    private static final String STORAGE_DIRECTORY_TAG = "__storage_directory";
+
+    // Setting Key Constants
+    public static final String STORAGE_DIRECTORY_KEY = "pref_key__storage_directory";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +31,76 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
     }
 
+    @Override
+    public void onFolderSelection(@NonNull FolderChooserDialog dialog, @NonNull File folder) {
 
+        if (dialog.getTag() != null) {
+            switch (dialog.getTag()) {
+                case STORAGE_DIRECTORY_TAG:
+
+                    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                    SharedPreferences.Editor editor = preferences.edit();
+
+                    editor.putString(STORAGE_DIRECTORY_KEY, folder.getAbsolutePath());
+                    editor.apply();
+            }
+        }
+    }
+
+    @Override
+    public void onFolderChooserDismissed(@NonNull FolderChooserDialog dialog) {
+
+    }
+
+    public static class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.preferences);
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            Preference preference = findPreference(STORAGE_DIRECTORY_KEY);
+            preference.setSummary(preferences.getString(STORAGE_DIRECTORY_KEY, ""));
+
+            preferences.registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            PreferenceManager.getDefaultSharedPreferences(getActivity()).unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+
+            if (isAdded() && preference.hasKey()) {
+                switch (preference.getKey()) {
+                    case STORAGE_DIRECTORY_KEY:
+                        new FolderChooserDialog.Builder(getActivity())
+                                .chooseButton(R.string.md_choose_label)  // changes label of the choose button
+                                .initialPath(Environment.getExternalStorageDirectory().getAbsolutePath())  // changes initial path, defaults to external storage directory
+                                .tag(STORAGE_DIRECTORY_TAG)
+                                .goUpLabel(getString(R.string.settings__parent_dir)) // custom go up label, default label is "..."
+                                .allowNewFolder(true, 0)
+                                .show(((AppCompatActivity)getActivity()));
+
+                }
+            }
+
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            Preference preference = findPreference(STORAGE_DIRECTORY_KEY);
+            preference.setSummary(sharedPreferences.getString(key, ""));
+        }
+    }
 }
